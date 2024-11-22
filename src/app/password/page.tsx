b0b363d0440c83e6926378b8eeb0df3baba8b068
hardik-sharma-vanshiv <hardik.sharma@vanshiv.com>
2024-11-22 21:02:55 +0530

    Fixed Error

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -3,7 +3,6 @@ import InputField from "@/components/InputField";
 import ErrorToast from "@/components/toast/ErrorToast";
 import SuccessfulToast from "@/components/toast/SuccessfulToast";
 import { handleFormDataChange, validateForm } from "@/lib/helper";
-import { resetPassword } from "@/lib/service/user.service";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -51,31 +50,31 @@ const resetPasswordPage = () => {
     // }, [])
 
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        if (!validateForm(formData, setErrors)) {
-            return;
-        }
-        const signupData = new FormData();
-        signupData.append('new_password', formData.new_password);
-        signupData.append('confirm_password', formData.confirm_password);
-        signupData.append('token', "user_token");
-        try {
-            const response = await resetPassword(signupData);
-            if (response && response.user) {
-                console.log("User Data", response.user);
-                toast.custom((t) => (
-                    <SuccessfulToast t={t} message={"Logged in successfully"} />
-                ));
-                router.push('/developer/dashboard');
-            }
-        } catch (error: any) {
-            console.error("Error resetting password", error);
-            toast.custom((t) => (
-                <ErrorToast t={t} message={error?.response?.data?.error || "An error occurred"} />
-            ));
-        }
-    };
+    // const handleSubmit = async (e: React.FormEvent) => {
+    //     e.preventDefault();
+    //     if (!validateForm(formData, setErrors)) {
+    //         return;
+    //     }
+    //     const signupData = new FormData();
+    //     signupData.append('new_password', formData.new_password);
+    //     signupData.append('confirm_password', formData.confirm_password);
+    //     signupData.append('token', "user_token");
+    //     try {
+    //         const response = await resetPassword(signupData);
+    //         if (response && response.user) {
+    //             console.log("User Data", response.user);
+    //             toast.custom((t) => (
+    //                 <SuccessfulToast t={t} message={"Logged in successfully"} />
+    //             ));
+    //             router.push('/developer/dashboard');
+    //         }
+    //     } catch (error: any) {
+    //         console.error("Error resetting password", error);
+    //         toast.custom((t) => (
+    //             <ErrorToast t={t} message={error?.response?.data?.error || "An error occurred"} />
+    //         ));
+    //     }
+    // };
 
     return (
         <section className=" bg-white overflow-hidden text-black">
@@ -86,7 +85,7 @@ const resetPasswordPage = () => {
                     </a>
                     <h2 className="mb-4 text-6xl md:text-4xl text-center font-bold font-heading tracking-px-n leading-tight">Welcome Back</h2>
                     <p className="mb-12 font-medium text-lg text-gray-600 leading-normal">Lorem ipsum dolor sit amet, to the con adipiscing. Volutpat tempor to the condim entum.</p>
-                    <form onSubmit={handleSubmit} className="w-full flex flex-col gap-6">
+                    <form onSubmit={() => { }} className="w-full flex flex-col gap-6">
                         <InputField
                             type="password"
                             label={"New Password"}
